6c4413642411ca26e82faab86bd49eb4d156088b
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-20 22:20:47 +0100

    Update

--- grbl-Status-light-tower-M5-Atom-Matrix/src/main.cpp
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/main.cpp
@@ -3,25 +3,25 @@
 // Passtrouh bidirectional UART CNC <--> UART USB all Commands & Status
 // GRBL Status to GPIO  // GRBL Status to Display // GRBL Status to ESPNow ... and more
 
-#include <Arduino.h>                                                                              // Arduino Include Header
-#include "grbl_hub_local_config.h"                                                                // --> Config for all Pin's for your Board!!!!
-#include "grbl_hub_variables.h"                                                                   // All Variables for GRBL-Hub
-#include "grbl_hub_uart.h"                                                                        // Code for UART from / to Local Board to USB
-#include "grbl_hub_parser.h"                                                                      // Code for GRBL 1.1 Parser to Status struct Variables
-#include "grbl_hub_local_control.h"                                                               // Code for GRBL 1.1 Status to Local GPIO --> for example LED or Buzzer
-#include "m5_atom_matrix.h"                                                                       // ATOM Matrix Include     --> disable for no M5Stack Atom Matrix
+#include <Arduino.h>                                            // Arduino Include Header
+#include "grbl_hub_local_config.h"                              // --> Config for all Pin's for your Board!!!!
+#include "grbl_hub_variables.h"                                 // All Variables for GRBL-Hub
+#include "grbl_hub_uart.h"                                      // Code for UART from / to Local Board to USB
+#include "grbl_hub_parser.h"                                    // Code for GRBL 1.1 Parser to Status struct Variables
+#include "grbl_hub_local_control.h"                             // Code for GRBL 1.1 Status to Local GPIO --> for example LED or Buzzer
+#include "m5_atom_matrix.h"                                     // ATOM Matrix Include     --> disable for no M5Stack Atom Matrix
 
-void setup() {                                                                                    // SETUP
-  uart_to_usb_setup();                                                                            // Setup for UART on USB Local 
-  uart_cnc_setup();                                                                            // Setup for UART to CNC 
-  m5_atom_matrix_setup();
+void setup() {                                                  // SETUP
+  uart_to_usb_setup();                                          // Setup for UART on USB Local 
+  uart_cnc_setup();                                             // Setup for UART to CNC 
+  m5_atom_matrix_setup();                                       // Setup M5 Atom Matrix
 }
 
-void loop() {                                                                                     // Loop
-  grbl_status_update_request();                                                                   // Poll the CNC Controller with ?
-  uart_usb_read();                                                                                // read data from USB Uart
-  uart_cnc_read();                                                                                // read data from CNC Uart
-  grbl_Message_main_parser();                                                                     // Parse GRBL all GRBL Status information's
-  m5_atom_matrix_update();
+void loop() {                                                    // Loop
+  grbl_status_update_request();                                  // Poll the CNC Controller with ?
+  uart_usb_read();                                               // read data from USB Uart
+  uart_cnc_read();                                               // read data from CNC Uart
+  grbl_Message_main_parser();                                    // Parse GRBL all GRBL Status information's
+  m5_atom_matrix_update();                                       // Update the M5 Matrix Display
 }
 

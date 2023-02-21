b0774df140fdbeca523205b88bf3c0c861c99833
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-21 22:04:06 +0100

    Commets

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_parser.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_parser.h
@@ -8,25 +8,25 @@ Message_Type grbl_Message_Type(String grbl_message){                            
 // *************************** Current Maschine State  ************************************//
 State grbl_Maschine_State(String grbl_message){                                            // Return Maschine as State
   grbl_message.toUpperCase();                                                              // message to Upper Case
-  if (grbl_message.startsWith("<IDLE")){return State::Idle ;}                              // start with ok        --> Return 1 = OK  
-  else if (grbl_message.startsWith("<HOMING")){return State::Homing;}                                               // start with chevrons  --> Return 2 = Real-time status report data
-  else if (grbl_message.startsWith("<RUN")){return State::Run;}                            // start with chevrons  --> Return 2 = Real-time status report data
-  else if (grbl_message.startsWith("<JOG")){return State::Jog;}                            // start with ALARM:    --> Return 3 = Alarm$
-  else if (grbl_message.startsWith("<HOLD")){return State::Hold;}                          // start with ALARM:    --> Return 3 = Alarm
-  else if (grbl_message.startsWith("<DOOR")){return State::SafetyDoor;}                                          // start with ALARM:    --> Return 3 = Alarm
-  else if (grbl_message.startsWith("<ALARM")){return State::Alarm;}                                          // start with ALARM:    --> Return 3 = Alarm
-  else if (grbl_message.startsWith("<SLEEP")){return State::Sleep;}                                          // start with error:    --> Return 4 = error
+  if (grbl_message.startsWith("<IDLE")){return State::Idle ;}                              // start with <IDLE
+  else if (grbl_message.startsWith("<HOMING")){return State::Homing;}                      // start with <Homing
+  else if (grbl_message.startsWith("<RUN")){return State::Run;}                            // start with <Run
+  else if (grbl_message.startsWith("<JOG")){return State::Jog;}                            // start with <Jog
+  else if (grbl_message.startsWith("<HOLD")){return State::Hold;}                          // start with <Hold
+  else if (grbl_message.startsWith("<DOOR")){return State::SafetyDoor;}                    // start with <SafetyDoor
+  else if (grbl_message.startsWith("<ALARM")){return State::Alarm;}                        // start with <Alarm
+  else if (grbl_message.startsWith("<SLEEP")){return State::Sleep;}                        // start with <Sleep
   else {return State::Sleep; }                                
 }
-// *************************** Message without <>  ************************************//
-String grbl_message_clean_chevrons(String grbl_message){                                          // Remove chevrons and message to Upper Case
-  int startPos = grbl_message.indexOf("<");                                                       // Pos <
-  int endPos = grbl_message.indexOf(">");                                                         // Pos >
-  grbl_message = grbl_message.substring(startPos+1, endPos-0);                                    // Substring without chevrons
-  return grbl_message;                                                                            // Return without chevrons
+// ******************************* Message without <>  ************************************//
+String grbl_message_clean_chevrons(String grbl_message){                                   // Remove chevrons and message to Upper Case
+  int startPos = grbl_message.indexOf("<");                                                // Pos <
+  int endPos = grbl_message.indexOf(">");                                                  // Pos >
+  grbl_message = grbl_message.substring(startPos+1, endPos-0);                             // Substring without chevrons
+  return grbl_message;                                                                     // Return without chevrons
 }
 
-// *************************** Main Parser  ************************************//
+// ************************************** Main Parser  ************************************//
 void grbl_Message_main_parser(){                                                                  // GRBL Parser main
   if (uart_cnc_read_raw.Message_Available == true) {                                              // Check if raw Message String Available
     uart_cnc_read_raw.Message_Typ = grbl_Message_Type(uart_cnc_read_raw.Message);

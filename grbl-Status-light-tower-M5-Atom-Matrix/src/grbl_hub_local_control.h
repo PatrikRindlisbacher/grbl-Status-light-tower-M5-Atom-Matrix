1b0c9c9ed3b6d82431478ca0ff8301d970fb0ea1
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-21 09:55:17 +0100

    GPIO  Setup

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_control.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_control.h
@@ -6,17 +6,15 @@ void grbl_staus_to_gpio_update(){
   }
 }
 
-
-
 // ***************************** L O C A L  G P I O   S E T U P *************************//
 void grbl_staus_to_gpio_setup(){
-  if (grbl_hub_config.pin_Light_Tower_red !=0){       // Light Tower red                                                                                             // if defines the connection for MIST Gpio Output
-
+  if (grbl_hub_config.pin_Light_Tower_red !=0){                   // Light Tower red                                                                                             // if defines the connection for MIST Gpio Output
+    pinMode(grbl_hub_config.pin_Light_Tower_red, OUTPUT);         // sets the digital pin as output
   }
-  if (grbl_hub_config.pin_Light_Tower_orange !=0){    // Light Tower orange                                                           // if defines the connection for MIST Gpio Output
-
+  if (grbl_hub_config.pin_Light_Tower_orange !=0){                // Light Tower orange                                                           // if defines the connection for MIST Gpio Output
+    pinMode(grbl_hub_config.pin_Light_Tower_orange, OUTPUT);      // sets the digital pin as output
   }
-  if (grbl_hub_config.pin_Light_Tower_orange !=0){    // Light Tower green                                                           // if defines the connection for MIST Gpio Output
-
+  if (grbl_hub_config.pin_Light_Tower_green !=0){                 // Light Tower green                                                           // if defines the connection for MIST Gpio Output
+    pinMode(grbl_hub_config.pin_Light_Tower_green, OUTPUT);       // sets the digital pin as output
   }
 }

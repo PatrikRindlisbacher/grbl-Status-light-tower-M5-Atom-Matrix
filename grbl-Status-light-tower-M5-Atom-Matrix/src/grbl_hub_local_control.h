d8578ae1849cefa8c2281fbdee29f2965e8a735a
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-21 09:51:04 +0100

    GPIO Sertup

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_control.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_control.h
@@ -8,9 +8,15 @@ void grbl_staus_to_gpio_update(){
 
 
 
-
+// ***************************** L O C A L  G P I O   S E T U P *************************//
 void grbl_staus_to_gpio_setup(){
-  if (grbl_hub_config.pin_MIST_ON_Info_Output !=0){                                                             // if defines the connection for MIST Gpio Output
+  if (grbl_hub_config.pin_Light_Tower_red !=0){       // Light Tower red                                                                                             // if defines the connection for MIST Gpio Output
+
+  }
+  if (grbl_hub_config.pin_Light_Tower_orange !=0){    // Light Tower orange                                                           // if defines the connection for MIST Gpio Output
+
+  }
+  if (grbl_hub_config.pin_Light_Tower_orange !=0){    // Light Tower green                                                           // if defines the connection for MIST Gpio Output
 
   }
 }

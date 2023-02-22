a46a086dc81848856edd4f15e01467a79ff1d73b
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-22 10:51:00 +0100

    comments

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_control.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_control.h
@@ -1,28 +1,29 @@
 #include <Arduino.h>
 
-// **********************  T I M E R  F O R  B L I N K  & F L A S H **********************//
+// **********************  T I M E R  F O R  B L I N K   **********************//
 bool GPIO_state_blink(){
   if ( millis() > blink_and_flash.last_blink_start_millis + blink_and_flash.blink_duration_on + blink_and_flash.blink_duration_off){
     blink_and_flash.last_blink_start_millis= millis();
-    return HIGH;
+    return HIGH;                                                              // Response GPIO High
   }
   if (millis() < blink_and_flash.last_blink_start_millis + blink_and_flash.blink_duration_on){
-    return HIGH;
+    return HIGH;                                                              // Response GPIO High
   }
   else {
-    return LOW;
+    return LOW;                                                               // Response GPIO Low
   }
 }
+// **********************  T I M E R  F O R   F L A S H **********************//
 bool GPIO_state_flash(){
   if ( millis() > blink_and_flash.last_flash_start_millis + blink_and_flash.flash_duration_on + blink_and_flash.flash_duration_off){
     blink_and_flash.last_flash_start_millis= millis();
-    return HIGH;
+    return HIGH;                                                              // Response GPIO High
   }
   if (millis() < blink_and_flash.last_flash_start_millis + blink_and_flash.flash_duration_on){
-    return HIGH;
+    return HIGH;                                                              // Response GPIO High
   }
   else {
-    return LOW;
+    return LOW;                                                               // Response GPIO Low
   }
 }
 
@@ -48,17 +49,17 @@ bool current_GPIO_Output_State(GPIO_Output_Mode GPIO_Mode){
 // *************************** L O C A L  G P I O   U P D A T E  *************************//
 void grbl_status_to_gpio_update(){
   bool GPIO_New_State;
-  if (grbl_hub_config.pin_Light_Tower_red !=0){                         // Light Tower RED                                                        // if defines the connection for MIST Gpio Output
-    GPIO_New_State = current_GPIO_Output_State(light_tower_Status.red_lamp);
-    digitalWrite(grbl_hub_config.pin_Light_Tower_red, GPIO_New_State);  //
+  if (grbl_hub_config.pin_Light_Tower_red !=0){                                           // Light Tower RED                                                        // if defines the connection for MIST Gpio Output
+    GPIO_New_State = current_GPIO_Output_State(light_tower_Status.red_lamp);              // determine the current status
+    digitalWrite(grbl_hub_config.pin_Light_Tower_red, GPIO_New_State);                    // Set the current Status to GPIO 
   }
-  if (grbl_hub_config.pin_Light_Tower_orange !=0){                         // Light Tower Oragnge                                                        // if defines the connection for MIST Gpio Output
-    GPIO_New_State = current_GPIO_Output_State(light_tower_Status.orange_lamp);
-    digitalWrite(grbl_hub_config.pin_Light_Tower_orange, GPIO_New_State);  //
+  if (grbl_hub_config.pin_Light_Tower_orange !=0){                                        // Light Tower Oragnge                                                        // if defines the connection for MIST Gpio Output
+    GPIO_New_State = current_GPIO_Output_State(light_tower_Status.orange_lamp);           // determine the current status
+    digitalWrite(grbl_hub_config.pin_Light_Tower_orange, GPIO_New_State);                 // Set the current Status to GPIO  
   }
-  if (grbl_hub_config.pin_Light_Tower_green !=0){                         // Light Tower Green                                                        // if defines the connection for MIST Gpio Output
-    GPIO_New_State = current_GPIO_Output_State(light_tower_Status.green_lamp);
-    digitalWrite(grbl_hub_config.pin_Light_Tower_green, GPIO_New_State);  //
+  if (grbl_hub_config.pin_Light_Tower_green !=0){                                         // Light Tower Green                                                        // if defines the connection for MIST Gpio Output
+    GPIO_New_State = current_GPIO_Output_State(light_tower_Status.green_lamp);            // determine the current status
+    digitalWrite(grbl_hub_config.pin_Light_Tower_green, GPIO_New_State);                  // Set the current Status to GPIO 
   }
 }
 

c48d5a12dcbccbe83d302c07fb9faa68e7b4f053
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-22 12:15:50 +0100

    improvements

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_light_tower.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_light_tower.h
@@ -1,50 +1,51 @@
 #include <Arduino.h>
 
-// ************************** L I G H T  T O W E R  S T A T U S  *************************//
+//******************* L I G H T  T O W E R  S T A T U S  *********************//
+//******Here the machine status is assigned to the GPIO_Output_Conditions ****//
 void light_tower_status_update(){
-  if (Maschine_State.State_current== State::Idle){                          // Idle = green on
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;             // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::on;               // green
+  if (Maschine_State.State_current== State::Idle){                            // Idle = green on
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                  // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;               // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::on;                 // green    GPIO Conditions
   }
-  else if (Maschine_State.State_current== State::Alarm){                    // Alarm = red flashing
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::flashing;           // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;             // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;              // green
+  else if (Maschine_State.State_current== State::Alarm){                      // Alarm = red flashing
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::flashing;             // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;               // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions
   }
-  else if (Maschine_State.State_current== State::CheckMode){                // Checkmode = red blinking
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::blinking;           // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;             // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;    
+  else if (Maschine_State.State_current== State::CheckMode){                  // Checkmode = red blinking
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::blinking;             // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;               // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions   
   }
-  else if (Maschine_State.State_current== State::Run){                      // Run = orange on
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::on;              // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;              // green
+  else if (Maschine_State.State_current== State::Run){                        // Run = orange on
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                  // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::on;                // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions
   }
-  else if (Maschine_State.State_current== State::Hold){                     // Hold = orange on & red flashing
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::flashing;           // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::on;              // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;              // green
+  else if (Maschine_State.State_current== State::Hold){                       // Hold = orange on & red flashing
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::flashing;             // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::on;                // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions
   }
-  else if (Maschine_State.State_current== State::Jog){                      // Jog = orange blinking
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::blinking;        // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;              // green
+  else if (Maschine_State.State_current== State::Jog){                        // Jog = orange blinking
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                  // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::blinking;          // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions
   }
-  else if (Maschine_State.State_current== State::SafetyDoor){               // SafetyDoor = red flashing
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::flashing;           // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;             // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;              // green
+  else if (Maschine_State.State_current== State::SafetyDoor){                 // SafetyDoor = red flashing
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::flashing;             // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;               // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions
   }
-  else if (Maschine_State.State_current== State::Sleep){                    // Sleep = all off
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;             // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;              // green
+  else if (Maschine_State.State_current== State::Sleep){                      // Sleep = all off
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::off;                  // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;               // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions
   }
-  else if (Maschine_State.State_current== State::ConfigAlarm){              // Config Alarm = red blinking
-    light_tower_Status.red_lamp=GPIO_Output_Conditions::blinking;           // red 
-    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;             // orange
-    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;              // green
+  else if (Maschine_State.State_current== State::ConfigAlarm){                // Config Alarm = red blinking
+    light_tower_Status.red_lamp=GPIO_Output_Conditions::blinking;             // red      GPIO Conditions
+    light_tower_Status.orange_lamp=GPIO_Output_Conditions::off;               // orange   GPIO Conditions
+    light_tower_Status.green_lamp=GPIO_Output_Conditions::off;                // green    GPIO Conditions
   }
 }

54877cef8bd7e6ea7aba023e28d0f0fb446a6761
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-22 12:09:04 +0100

    Comments

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_variables.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_variables.h
@@ -28,7 +28,7 @@ enum class Message_Type : uint8_t {
   startup,            // >G54G20:ok : The open chevron indicates startup line execution. The :ok suffix shows it executed correctly without adding an unmatched ok response on a new line.
 };
 
-//************************** R A W   D A T A  G R B L  M E S S A G E  ********************//
+//************************** R A W   D A T A  G R B L  M E S S A G E  ********************// structure type
 struct raw_uart_cnc_read_t {                                                              // Alle Status Variablen 
   String  Message = "";                                                                   // Message from CNC UART
   boolean Message_Available = false;                                                      // New Message Available
@@ -40,14 +40,14 @@ struct raw_uart_cnc_read_t {                                                    
   bool heart_beat_last = false;                                                           // Heart Beat / Watch Dog
  } uart_cnc_read_raw ;
 
-//******************************** M A S C H I N E  S T A T E  ************************** //
+//******************************** M A S C H I N E  S T A T E  ************************** // structure type
 struct Maschine_State_t {                                                                 // Maschine Status Information
   State State_current = State::Sleep;                                                     // Start State Sleep
   State State_old = State::Sleep;                                                         // Maschine State old
   String  Machine_State_String = "";                                                      // Maschine State For language translations
 }Maschine_State ;
 
-//********************* G P I O  O U T P U T   C O N D I T I O N S  ********************* //
+//********************* G P I O  O U T P U T   C O N D I T I O N S  ********************* // enum class
 enum class GPIO_Output_Conditions : uint8_t {                                             // GPIO CONDITIONS  ON OFF BLINK FLASH
     off = 0,                                                                              // GPIO / lamp / LED  is off
     on ,                                                                                  // GPIO / lamp / LED  is on
@@ -57,9 +57,9 @@ enum class GPIO_Output_Conditions : uint8_t {                                   
 
 //******************************** L I G H T  T O W E R   S T A T E  *********************// structure type
 struct light_tower_Status_t {                                                             // Light Tower Status 
-  GPIO_Output_Conditions red_lamp;                                                        // CONDITION RED 
-  GPIO_Output_Conditions orange_lamp;                                                     // CONDITION Organge 
-  GPIO_Output_Conditions green_lamp;                                                      // CONDITION Green
+  GPIO_Output_Conditions red_lamp;                                                        // CONDITION RED      (ON OFF BLINK FLASH)
+  GPIO_Output_Conditions orange_lamp;                                                     // CONDITION Organge  (ON OFF BLINK FLASH)
+  GPIO_Output_Conditions green_lamp;                                                      // CONDITION Green    (ON OFF BLINK FLASH)
 } light_tower_Status ;                                                                    // 
 
 
@@ -69,6 +69,6 @@ struct blink_and_flash_t {                                                      
   uint32_t blink_duration_off = 1000;                                                     // Blink duration OFF
   uint32_t flash_duration_on = 100;                                                       // Flash duration ON
   uint32_t flash_duration_off = 1900;                                                     // Flash duration OFF
-  uint32_t last_blink_start_millis = 0;                                                   // Last Start Blink
-  uint32_t last_flash_start_millis = 0;                                                   // Last Start Flash
+  uint32_t last_blink_start_millis = 0;                                                   // Last Start Blink in millis seconds
+  uint32_t last_flash_start_millis = 0;                                                   // Last Start Flash in millis seconds
  } blink_and_flash ;

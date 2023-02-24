da2fa516cf876df8a9e291f3b0e6f68cf45aa119
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-24 20:37:37 +0100

    Comments

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_rgb_leds.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_rgb_leds.h
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 
-//***************************** RGB LED needed Update *************  //
-bool grbl_hub_rgb_led_update_needed(){
-  if (uart_cnc_read_raw.last_string_reveive_millis + 1000 < millis()){
-    Maschine_State.State_current = State::Alarm;
+//***************************** RGB LED needed Update **************************  //
+bool grbl_hub_rgb_led_update_needed(){                                            // update needed for RGB Led's
+  if (uart_cnc_read_raw.last_string_reveive_millis + 1000 < millis()){            // if no message arrives for a long time
+    Maschine_State.State_current = State::Alarm;                                  // change Maschine State = Alarm
   }
   // if (uart_cnc_read_raw.heart_beat_current != uart_cnc_read_raw.heart_beat_last){
   //   uart_cnc_read_raw.heart_beat_last = uart_cnc_read_raw.heart_beat_current;
   //   return true;
   // }  
-  if (Maschine_State.State_old != Maschine_State.State_current){
-    Maschine_State.State_old = Maschine_State.State_current;
-    uart_cnc_read_raw.heart_beat_last = uart_cnc_read_raw.heart_beat_current;
-    return true;
+  if (Maschine_State.State_old != Maschine_State.State_current){                  // if change Maschine State
+    Maschine_State.State_old = Maschine_State.State_current;                      // old = current State
+    uart_cnc_read_raw.heart_beat_last = uart_cnc_read_raw.heart_beat_current;     //
+    return true;                                                                  //
   }
   else {
-      return false;
+      return false;                                                               //
   }
 }
 
@@ -23,34 +23,34 @@ bool grbl_hub_rgb_led_update_needed(){
 #ifdef RGB_LED_1x1
 void rgb_led_1x1_matrix_update() {                                              // RGB Led update
   if (Maschine_State.State_current == State::Idle){                             // Idle
-    M5.dis.drawpix(0,0x00ff00);
+    M5.dis.drawpix(0,0x00ff00);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::Alarm){                        // Alarm
-    M5.dis.drawpix(0,0xff0000);
+    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::CheckMode){                    // Checkmode
-    M5.dis.drawpix(0,0xff0000);
+    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::Homing){                       // Homing
-    M5.dis.drawpix(0,0xffaa00);
+    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::Run){                          // Run
-    M5.dis.drawpix(0,0xffaa00);
+    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::Hold){                         // Hold
-    M5.dis.drawpix(0,0xffaa00);
+    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::Jog){                          // Jog
-    M5.dis.drawpix(0,0xffaa00);
+    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::SafetyDoor){                   // SafetyDoor
-    M5.dis.drawpix(0,0xff0000);
+    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::Sleep){                        // Sleep
-    M5.dis.drawpix(0,0x000000);
+    M5.dis.drawpix(0,0x000000);                                                 // LED Color Update
   }
   else if(Maschine_State.State_current == State::ConfigAlarm){                  // ConfigAlarm
-    M5.dis.drawpix(0,0xff0000);
+    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
   }
 }
 #endif
@@ -152,13 +152,14 @@ void rgb_led_5x5_matrix_update() {                                          // R
   }
 #endif
 
-void grbl_hub_rgb_led_update(){                                             // rgb LED update
-  bool update_needed = grbl_hub_rgb_led_update_needed();                    // is led update needed
-  if (update_needed){                                                       // LED Update needed 
-    #ifdef RGB_LED_5x5                                                      // BuiltFlag RGB_LED_5x5
-      rgb_led_5x5_matrix_update();                                          // Update 5x5 Led Matrix
-    #elif defined(RGB_LED_1x1)                                              // BuiltFlag RGB_LED_5x5
-      rgb_led_1x1_matrix_update();                                          // Update 1x1 Led
-    #endif                                                                  // 
+//****************************** RGB Led  Update *******************************  // RGB Led update
+void grbl_hub_rgb_led_update(){                                                   // rgb LED update
+  bool update_needed = grbl_hub_rgb_led_update_needed();                          // is led update needed
+  if (update_needed){                                                             // LED Update needed 
+    #ifdef RGB_LED_5x5                                                            // BuiltFlag RGB_LED_5x5
+      rgb_led_5x5_matrix_update();                                                // Update 5x5 Led Matrix
+    #elif defined(RGB_LED_1x1)                                                    // BuiltFlag RGB_LED_5x5
+      rgb_led_1x1_matrix_update();                                                // Update 1x1 Led
+    #endif                                                                        // 
   }
 }

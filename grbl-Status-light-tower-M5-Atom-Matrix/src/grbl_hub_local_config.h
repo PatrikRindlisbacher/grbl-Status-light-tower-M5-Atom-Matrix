14ac46d980eaa66a3e36fbfcba93e07836bae9a4
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-24 12:00:07 +0100

    first debug flags in Platformio.ini

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_config.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_local_config.h
@@ -1,11 +1,11 @@
 #include <Arduino.h>
-
 #include "M5Atom.h"
 
+// All constants "FLAG_" are in the PlatformIO.ini as Debug_Flag defined.
 //*************** Local P I N   C O N F I G U R A T I O N **********//
 struct grbl_hub_pin_config_t {                                      // All Pin Variables 
-  int8_t pin_RXD_Uart_to_cnc = 26;                                  // RXD GPIO 26 grove weiss -->  to Bart Dring 6er CNC Controller  TX or GPIO 13 = TXD
-  int8_t pin_TXD_Uart_to_cnc = 32;                                  // TXD GPIO 32 grove gelb  -->  to Bart Dring 6er CNC Controller  RX or GPIO 14 = RXD
+  int8_t pin_RXD_Uart_to_cnc = FLAG_PIN_RXD_UART_TO_CNC;            // RXD GPIO 26 grove weiss -->  to Bart Dring 6er CNC Controller  TX or GPIO 13 = TXD
+  int8_t pin_TXD_Uart_to_cnc = FLAG_PIN_TXD_UART_TO_CNC;            // TXD GPIO 32 grove gelb  -->  to Bart Dring 6er CNC Controller  RX or GPIO 14 = RXD
   int8_t pin_Info_Output_Spindle_ON = 0;                            // GPIO Pin Info Spindel ON
   int8_t pin_Info_Output_MIST_ON = 0;                               // GPIO Pin Info MIST  Cooling (Nebel)
   int8_t pin_Info_Output_FLOOD_ON = 0;                              // GPIO Pin Info Flood Cooling (Kühlwasser-Flut)

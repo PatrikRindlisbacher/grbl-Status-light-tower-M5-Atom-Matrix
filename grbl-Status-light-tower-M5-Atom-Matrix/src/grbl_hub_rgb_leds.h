3cc9d796ef161305c6e8ad997b2bc06ade7713d7
CLE PolyTec Rindlisbacher Patrik <pr@cle.ch>
2023-02-24 20:46:35 +0100

    minors

--- grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_rgb_leds.h
+++ grbl-Status-light-tower-M5-Atom-Matrix/src/grbl_hub_rgb_leds.h
@@ -10,7 +10,7 @@ bool grbl_hub_rgb_led_update_needed(){                                          
   //   return true;
   // }  
   if (Maschine_State.State_old != Maschine_State.State_current){                  // if change Maschine State
-    Maschine_State.State_old = Maschine_State.State_current;                      // old = current State
+    Maschine_State.State_old = Maschine_State.State_current;                      // old State = current State
     uart_cnc_read_raw.heart_beat_last = uart_cnc_read_raw.heart_beat_current;     //
     return true;                                                                  //
   }
@@ -19,51 +19,51 @@ bool grbl_hub_rgb_led_update_needed(){                                          
   }
 }
 
-//****************************** RGB Led 5x5  Matrix Update ******************* // 1x1 Matrix RGB Led (M5 Atom-Lite)
-#ifdef RGB_LED_1x1
-void rgb_led_1x1_matrix_update() {                                              // RGB Led update
-  if (Maschine_State.State_current == State::Idle){                             // Idle
-    M5.dis.drawpix(0,0x00ff00);                                                 // LED Color Update
+//****************************** RGB Led 5x5  Matrix Update **********************// 1x1 Matrix RGB Led (M5 Atom-Lite)
+#ifdef RGB_LED_1x1                                                                // Built Flag --> RGB_LED_1x1
+void rgb_led_1x1_matrix_update() {                                                // RGB Led update
+  if (Maschine_State.State_current == State::Idle){                               // Idle
+    M5.dis.drawpix(0,0x00ff00);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::Alarm){                        // Alarm
-    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::Alarm){                          // Alarm
+    M5.dis.drawpix(0,0xff0000);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::CheckMode){                    // Checkmode
-    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::CheckMode){                      // Checkmode
+    M5.dis.drawpix(0,0xff0000);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::Homing){                       // Homing
-    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::Homing){                         // Homing
+    M5.dis.drawpix(0,0xffaa00);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::Run){                          // Run
-    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::Run){                            // Run
+    M5.dis.drawpix(0,0xffaa00);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::Hold){                         // Hold
-    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::Hold){                           // Hold
+    M5.dis.drawpix(0,0xffaa00);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::Jog){                          // Jog
-    M5.dis.drawpix(0,0xffaa00);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::Jog){                            // Jog
+    M5.dis.drawpix(0,0xffaa00);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::SafetyDoor){                   // SafetyDoor
-    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::SafetyDoor){                     // SafetyDoor
+    M5.dis.drawpix(0,0xff0000);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::Sleep){                        // Sleep
-    M5.dis.drawpix(0,0x000000);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::Sleep){                          // Sleep
+    M5.dis.drawpix(0,0x000000);                                                   // LED Color Update
   }
-  else if(Maschine_State.State_current == State::ConfigAlarm){                  // ConfigAlarm
-    M5.dis.drawpix(0,0xff0000);                                                 // LED Color Update
+  else if(Maschine_State.State_current == State::ConfigAlarm){                    // ConfigAlarm
+    M5.dis.drawpix(0,0xff0000);                                                   // LED Color Update
   }
 }
 #endif
 
-//****************************** RGB Led 5x5  Matrix Update **************  // 5x5 Matrix RGB Led (M5 Atom-Matrix)
+//****************************** RGB Led 5x5  Matrix Update **********************// 5x5 Matrix RGB Led (M5 Atom-Matrix)
 #ifdef RGB_LED_5x5
-void rgb_led_5x5_matrix_update() {                                          // RGB Led update
+void rgb_led_5x5_matrix_update() {                                                // RGB Led update
                                                    // 
-    // if (uart_cnc_read_raw.heart_beat_current == true){                            // Heart Beat
-    //   M5.dis.setBrightness(100) ;                                                 // Brightness 100%
+    // if (uart_cnc_read_raw.heart_beat_current == true){                         // Heart Beat
+    //   M5.dis.setBrightness(100) ;                                              // Brightness 100%
     // }
     // else {
-    //   M5.dis.setBrightness(80) ;                                                  // Brightness 80%
+    //   M5.dis.setBrightness(80) ;                                               // Brightness 80%
     // }
     
     if (Maschine_State.State_current == State::Idle){                             // Idle
@@ -152,7 +152,7 @@ void rgb_led_5x5_matrix_update() {                                          // R
   }
 #endif
 
-//****************************** RGB Led  Update *******************************  // RGB Led update
+//****************************** RGB Led  Update *********************************// RGB Led update
 void grbl_hub_rgb_led_update(){                                                   // rgb LED update
   bool update_needed = grbl_hub_rgb_led_update_needed();                          // is led update needed
   if (update_needed){                                                             // LED Update needed 
